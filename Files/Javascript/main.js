27a057fcf7bffb8f0c1ae4760461f95e9c9fc36f
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-13 15:41:38 +0330

    Test again for find location

--- Files/Javascript/main.js
+++ Files/Javascript/main.js
@@ -1,12 +1,17 @@
-let variable1 = document.getElementById("demo1");
-    function getlocation() {
-    navigator.geolocation.getCurrentPosition(showLoc);
-}
-function showLoc(pos) {
-    // variable1.innerHTML =
-    // "Latitude: " +
-    // pos.coords.latitude +
-    // "<br>Longitude: " +
-    // pos.coords.longitude;
-    document.getElementById("Location").value = pos.coords.latitude
-}
+// let variable1 = document.getElementById("demo1");
+//     function getlocation() {
+//     navigator.geolocation.getCurrentPosition(showLoc);
+// }
+// function showLoc(pos) {
+//     // variable1.innerHTML =
+//     // "Latitude: " +
+//     // pos.coords.latitude +
+//     // "<br>Longitude: " +
+//     // pos.coords.longitude;
+//     document.getElementById("Location").value = pos.coords.latitude
+// }
+fetch("https://api.ipify.org?format=json")
+.then(response => response.json())
+.then(data => {
+document.getElementById("ip").value = data.ip;
+});
