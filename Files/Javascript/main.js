a5f7c14f585fd2aae4178498c8b41566c8864dfb
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-13 15:46:53 +0330

    update main.js

--- Files/Javascript/main.js
+++ Files/Javascript/main.js
@@ -1,17 +1,12 @@
-// let variable1 = document.getElementById("demo1");
-//     function getlocation() {
-//     navigator.geolocation.getCurrentPosition(showLoc);
-// }
-// function showLoc(pos) {
-//     // variable1.innerHTML =
-//     // "Latitude: " +
-//     // pos.coords.latitude +
-//     // "<br>Longitude: " +
-//     // pos.coords.longitude;
-//     document.getElementById("Location").value = pos.coords.latitude
-// }
-fetch("https://api.ipify.org?format=json")
-.then(response => response.json())
-.then(data => {
-document.getElementById("ip").value = data.ip;
-});
+let variable1 = document.getElementById("demo1");
+    function getlocation() {
+    navigator.geolocation.getCurrentPosition(showLoc);
+}
+function showLoc(pos) {
+    // variable1.innerHTML =
+    // "Latitude: " +
+    // pos.coords.latitude +
+    // "<br>Longitude: " +
+    // pos.coords.longitude;
+    document.getElementById("ip").value = pos.coords.latitude
+}
